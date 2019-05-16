cfb9ce2de2e77c5ea017807843480ff72bee243e
coanor <coanor@gmail.com>
2019-05-16 11:52:15 +0800

    update: add logio

---
+++ logio/logio_test.go
@@ -0,0 +1,138 @@
+package logio
+
+import (
+	"log"
+	"testing"
+)
+
+func TestSetLog(t *testing.T) {
+	SetLog(`/tmp/test-set-log`, `debug`, true, false)
+
+	log.Printf("[debug] this is a test log")
+
+	SetLog(`/tmp/test-set-log`, `debug`, false, true)
+	log.Printf("[debug] this is a test log with json format")
+
+	SetLog(`/tmp/test-set-log`, `debug`, false, false)
+	log.Printf("[debug] this is a test log with json format and long file-name")
+
+	SetLog(`/tmp/test-set-log`, `debug`, false, true)
+	log.Printf("[debug] this is a test log with json format and short file-name")
+}
+
+func TestNoBackup(t *testing.T) {
+	lw, err := New(`z.log`, JsonFormat)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lw.Close()
+
+	log.SetOutput(lw)
+	log.SetFlags(log.Llongfile)
+
+	lw.SetLevel(Info)
+	lw.DisableBackup()
+
+	RotateSize = 32 * 1024 * 1024
+	i := 0
+	for {
+		log.Printf("[ERROR] log %d error", i)
+		log.Printf("[WARN] log %d warn", i)
+		i++
+	}
+}
+
+func TestLog(t *testing.T) {
+	lw, err := New(`y.log`, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lw.Close()
+
+	log.SetOutput(lw)
+	log.SetFlags(log.Llongfile | log.LstdFlags)
+
+	log.Printf("[debug] this is a debug message")
+	log.Printf("[info] this is a info message")
+	log.Printf("[error] this is a error message")
+	log.Printf("")
+	log.Printf("raw message")
+
+	type X struct {
+		a int
+		b string
+	}
+
+	x := X{
+		a: 42,
+		b: "hahah",
+	}
+
+	log.Printf("%+#v", x)
+	log.Printf("[info] %+#v", x)
+
+	lw.SetLevel(Info)
+	log.Printf("[debug] SHOULD-NOT-LOGGED: %+#v", x)
+	log.Printf("SHOULD-NOT-LOGGED: %+#v", x)
+
+	lw.SetLevel(Debug)
+
+	log.Printf("[debug] SHOULD-LOGGED: %+#v", x)
+	log.Printf("SHOULD-LOGGED: %+#v", x)
+
+	RotateSize = 32 * 1024 * 1024
+	i := 0
+	for {
+		log.Printf("[ERROR] log %d error", i)
+		log.Printf("[WARN] log %d warn", i)
+		i++
+	}
+}
+
+func TestJsonFormatLog(t *testing.T) {
+
+	lw, err := New(`x.log`, JsonFormat)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lw.Close()
+
+	log.SetOutput(lw)
+	log.SetFlags(log.Llongfile)
+
+	log.Printf("[debug] this is a debug message")
+	log.Printf("[info] this is a info message")
+	log.Printf("[error] this is a error message")
+	log.Printf("")
+	log.Printf("raw message")
+
+	type X struct {
+		a int
+		b string
+	}
+
+	x := X{
+		a: 42,
+		b: "hahah",
+	}
+
+	log.Printf("%+#v", x)
+	log.Printf("[info] %+#v", x)
+
+	lw.SetLevel(Info)
+	log.Printf("[debug] SHOULD-NOT-LOGGED: %+#v", x)
+	log.Printf("SHOULD-NOT-LOGGED: %+#v", x)
+
+	lw.SetLevel(Debug)
+
+	log.Printf("[debug] SHOULD-LOGGED: %+#v", x)
+	log.Printf("SHOULD-LOGGED: %+#v", x)
+
+	RotateSize = 32 * 1024 * 1024
+	i := 0
+	for {
+		log.Printf("[ERROR] log %d error", i)
+		log.Printf("[WARN] log %d warn", i)
+		i++
+	}
+}
---
+++ logio/writer.go
@@ -0,0 +1,242 @@
+package logio
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	Fatal = 4
+	Error = 3
+	Warn  = 2
+	Info  = 1
+	Debug = 0
+)
+
+var (
+	JsonFormat = "{\"timestamp\":%d, \"level\": \"%s\", \"message\": \"%s\"}\n"
+
+	levels = map[string]int{
+
+		`[debug] `: Debug,
+		`[DEBUG] `: Debug,
+		`[info] `:  Info,
+		`[INFO] `:  Info,
+		`[warn] `:  Warn,
+		`[WARN] `:  Warn,
+		`[error] `: Error,
+		`[ERROR] `: Error,
+		`[fatal] `: Fatal,
+		`[FATAL] `: Fatal,
+	}
+)
+
+var (
+	RotateSize = int64(1024 * 1024 * 32)
+	Backups    = 5
+)
+
+type RotateWriter struct {
+	lock          sync.Mutex
+	filename      string
+	fp            *os.File
+	curBytes      int64
+	backups       []string
+	format        string
+	level         int
+	beginAt       time.Time
+	disableBackup bool
+}
+
+func New(f, format string) (*RotateWriter, error) {
+	w := &RotateWriter{filename: f, format: format}
+
+	if err := w.rotate(); err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+func SetLog(f, level string, disableJsonFmt, disableLongFileName bool) {
+
+	if err := os.MkdirAll(path.Dir(f), os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	format := JsonFormat
+	if disableJsonFmt {
+		format = ""
+	}
+
+	rw, err := New(f, format)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logFlags := log.LstdFlags | log.Llongfile
+
+	if disableLongFileName {
+		logFlags = log.LstdFlags | log.Lshortfile
+	}
+
+	log.SetFlags(logFlags)
+
+	switch strings.ToUpper(level) {
+	case `DEBUG`:
+		rw.SetLevel(Debug)
+	case `INFO`:
+		rw.SetLevel(Info)
+	case `WARN`:
+		rw.SetLevel(Warn)
+	case `ERROR`:
+		rw.SetLevel(Error)
+	case `FATAL`:
+		rw.SetLevel(Fatal)
+	default:
+		rw.SetLevel(Debug)
+	}
+
+	log.SetOutput(rw)
+}
+
+func (w *RotateWriter) DisableBackup() {
+	w.disableBackup = true
+}
+
+func (w *RotateWriter) Close() error {
+	return w.fp.Close()
+}
+
+func (w *RotateWriter) SetLevel(l int) {
+	switch l {
+	case Fatal, Error, Warn, Info, Debug:
+		w.level = l
+	default:
+		w.level = Debug
+	}
+}
+
+func (w *RotateWriter) LogFiles(all bool) []string {
+	if all {
+		return append(w.backups, w.filename)
+	} else {
+		return []string{w.filename}
+	}
+}
+
+func fmtTs(t time.Time) string {
+	return fmt.Sprintf("%04d-%02d-%02d_%02d:%02d:%02d",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+}
+
+func (w *RotateWriter) rotate() error {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	var err error
+
+	if w.fp != nil {
+		err = w.fp.Close()
+		w.fp = nil
+		if err != nil {
+			return err
+		}
+	}
+
+	if fi, err := os.Stat(w.filename); err == nil {
+		if fi.Size() < RotateSize { // 继续追加日志
+			goto __open_file
+		} else {
+			// 切分出另一个日志
+			backupName := w.filename + `-` + fmtTs(w.beginAt) + `-` + fmtTs(time.Now())
+			if err := os.Rename(w.filename, backupName); err != nil {
+				return err
+			}
+
+			w.backups = append(w.backups, backupName)
+			w.beginAt = time.Now()
+
+			if len(w.backups) > Backups {
+				// 移除 backup 日志
+
+				_ = os.Remove(w.backups[0])
+				w.backups = w.backups[1:]
+			}
+		}
+	}
+
+__open_file:
+	w.fp, err = os.OpenFile(w.filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	// append 模式下, 当前文件大小计入 curBytes
+	if fi, err := os.Stat(w.filename); err == nil {
+		w.curBytes = fi.Size()
+	} else {
+		return err
+	}
+
+	return nil
+}
+
+func (w *RotateWriter) Write(data []byte) (int, error) {
+	if w.curBytes == 0 {
+		w.beginAt = time.Now()
+	}
+
+	if w.curBytes >= RotateSize && !w.disableBackup {
+		if err := w.rotate(); err != nil {
+			log.Printf("rotate failed: %s, ignored", err.Error())
+		}
+		w.curBytes = 0
+	}
+
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	n := 0
+	var err error
+
+	level := `debug`
+	key := `[debug] `
+	lv := Debug
+
+	for k, l := range levels {
+		if bytes.Contains(data, []byte(k)) {
+			level = k[1 : len(k)-2]
+			lv = l
+			key = k
+			break
+		}
+	}
+
+	if w.level > lv { // 当前日志被过滤掉
+		return 0, nil
+	}
+
+	switch w.format {
+	case JsonFormat:
+		data = data[0 : len(data)-1]                            // 去掉自带的换行
+		data = bytes.Replace(data, []byte(key), []byte(``), -1) // 去掉消息体中的 level
+		n, err = w.fp.Write([]byte(fmt.Sprintf(w.format,
+			time.Now().Unix(),
+			strings.ToUpper(level),
+			data)))
+	default: // 不带任何格式化，则按照默认情况输出
+		n, err = w.fp.Write(data)
+	}
+
+	if err == nil {
+		w.curBytes += int64(n)
+	}
+	return n, err
+}
