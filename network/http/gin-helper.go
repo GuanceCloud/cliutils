b99a5ca6c500b5be0190149cc6e94d505ee92a9c
coanor <coanor@gmail.com>
2019-06-05 18:36:19 +0800

    update: clean

--- network/http/gin-helper.go
+++ network/http/gin-helper.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"log"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -14,21 +13,10 @@ var hdrs = []string{
 
 func GetCliIP(c *gin.Context) (string, bool) {
 
-	log.Printf("[debug] %+#v", c.Request.Header)
-
 	for _, h := range hdrs {
 		addr := c.Request.Header.Get(h)
 		if len(addr) > 0 {
-
 			realIP := strings.Split(addr, `,`)[0] // 此处只取第一个 IP
-			if err != nil {
-				log.Printf("[error] invalid addr: %s", addr)
-				// ignore
-				continue
-			}
-
-			log.Printf("[debug] ip:port: %s:%s", realIP, port)
-
 			return realIP, true
 		}
 	}
