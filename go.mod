e13755ea306ff9159703fe54f0b50433a4d61cbd
coanor <coanor@gmail.com>
2019-11-08 04:25:27 +0000

    update: update uuid

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module gitlab.jiagouyun.com/cloudcare-tools/cliutils
 
-go 1.12
+go 1.13
 
 require github.com/satori/go.uuid v1.2.0
--- utils.go
+++ utils.go
@@ -87,7 +87,7 @@ func CreateRandomString(n int) string {
 
 func UUID(p string) string {
 	for {
-		id, _ := uuid.NewV4()
+		id := uuid.NewV4()
 
 		return p + id.String()
 	}
