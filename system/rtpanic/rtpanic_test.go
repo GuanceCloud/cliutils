47c1bbacb8b829030b12d1c0b0472c60440d1922
coanor <coanor@gmail.com>
2020-08-18 07:27:06 +0000

    update: 	[*] refact rtpanic

---
+++ system/rtpanic/rtpanic_test.go
@@ -0,0 +1,31 @@
+package rtpanic
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	//"time"
+)
+
+func TestRecover(t *testing.T) {
+	var f RecoverCallback
+	panicCnt := 0
+
+	f = func(trace []byte, err error) {
+		defer Recover(f, nil)
+
+		if trace != nil {
+			panicCnt++
+			log.Printf("try panic(at %d), err: %v, trace: %s", panicCnt, err, string(trace))
+			if panicCnt >= 1 {
+				return
+			}
+		}
+
+		panic(fmt.Errorf("panic error"))
+	}
+
+	f(nil, nil)
+
+	//time.Sleep(time.Second)
+}
