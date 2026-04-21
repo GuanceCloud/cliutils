2e1a99498eb33c785922bb7729c2440916453298
songlq <31207055+songlonqi-java@users.noreply.github.com>
2026-04-21 15:16:30 +0800

    grpc

--- vendor/github.com/GuanceCloud/pipeline-go/ptinput/funcs/fn_create_point.go
+++ vendor/github.com/GuanceCloud/pipeline-go/ptinput/funcs/fn_create_point.go
@@ -197,8 +197,6 @@ func ptCategory(cat string) point.Category {
 		return point.Security
 	case point.SDialTesting, point.CDT:
 		return point.DialTesting
-	case point.SAgentLLM, point.CAgentLLM:
-		return point.AgentLLM
 	}
 	return point.UnknownCategory
 }
